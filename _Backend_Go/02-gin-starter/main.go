527728e8d6520d00c9751bc6c13433f6f903c5a0
kienpham07 <kienpham121007.work@gmail.com>
2026-05-28 17:13:08 +0700

    05_28_2026

--- _Backend_Go/02-gin-starter/main.go
+++ _Backend_Go/02-gin-starter/main.go
@@ -9,5 +9,34 @@ func main() {
 		ctx.JSON(200, gin.H{"message": "Hello world", "number": 67})
 	})
 
+	r.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"data": "List of users"})
+	})
+
+	r.GET("/user/:user_id", func(ctx *gin.Context) {
+		user_id := ctx.Param("user_id") // Return string
+		ctx.JSON(200, gin.H{
+			"data":    "User information",
+			"user_id": user_id,
+		})
+	})
+
+	r.GET("/products", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"data": "List of products"})
+	})
+
+	r.GET("/product/:product_name", func(ctx *gin.Context) {
+		product_name := ctx.Param("product_name")
+		price := ctx.Query("price")
+		color := ctx.Query("color") // Query parameter is optional
+
+		ctx.JSON(200, gin.H{
+			"data":         "Product information",
+			"product_name": product_name,
+			"price":        price,
+			"color":        color,
+		})
+	})
+
 	r.Run(":8080")
 }
